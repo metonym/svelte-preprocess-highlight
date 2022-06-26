4a0b367c3dfc5a63ff8aae874d98b64d9ae9eced
Eric Liu <ericyl.us@gmail.com>
2022-06-26 11:27:55 -0700

    test: restructure test suite, reset console.log mock after each test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,145 +1,151 @@
-import { expect, test, vi } from "vitest";
-import * as API from "../src";
+import { afterEach, describe, expect, test, vi } from "vitest";
 import type { Processed } from "svelte/types/compiler/preprocess";
+import * as API from "../src";
 
-test("API", () => {
-  expect(Object.keys(API)).toMatchInlineSnapshot(`
-    [
-      "highlight",
-    ]
-  `);
-});
+describe("svelte-preprocess-highlight", () => {
+  const consoleLog = vi.spyOn(console, "log");
 
-const p = (
-  content: string,
-  filename: string = "App.svelte",
-  options?: Parameters<typeof API.highlight>[0]
-) => (API.highlight(options).markup?.({ content, filename }) as undefined | Processed)?.code;
+  afterEach(() => {
+    consoleLog.mockReset();
+  });
 
-const TYPESCRIPT_MESSY = `<pre data-language="typescript">{'const a= (b:number) =>4'}</pre>`;
-const TYPESCRIPT_STRING = `<pre data-language="typescript">{'const a = (b: number) => 4'}</pre>`;
-const TYPESCRIPT_QUASIS = `<pre data-language="typescript">{\`const a = (b: number) => 4\`}</pre>`;
-const TYPESCRIPT_QUASIS_LINE_BREAKS = `<pre data-language="typescript">
+  test("API", () => {
+    expect(Object.keys(API)).toMatchInlineSnapshot(`
+      [
+        "highlight",
+      ]
+    `);
+  });
 
-{\`const a = (b: number) => 4\`}
-</pre>`;
-const TYPESCRIPT_NO_CODE = `<pre data-language="typescript" />`;
-const TYPESCRIPT_NO_LANGUAGE = `<pre>{'const a = (b: number) => 4'}</pre>`;
-const TYPESCRIPT_FORMATTING_ERROR = `<pre data-language="typescript">{'const a= b:number) =>4'}</pre>`;
-const INVALID_LANG = `<pre data-language="hypescript">{'hype'}</pre>`;
-const SVELTE = `<pre data-language="svelte">{\`
-<script>
-  let count = 0;
-<\/script>
+  const p = (
+    content: string,
+    filename: string = "App.svelte",
+    options?: Parameters<typeof API.highlight>[0]
+  ) => (API.highlight(options).markup?.({ content, filename }) as undefined | Processed)?.code;
 
-{@html count}
-\`}
-</pre>`;
-const HTML = `<pre data-language="html">{\`
-<script>
-  let count = 0;
-<\/script>
+  const TYPESCRIPT_MESSY = `<pre data-language="typescript">{'const a= (b:number) =>4'}</pre>`;
+  const TYPESCRIPT_STRING = `<pre data-language="typescript">{'const a = (b: number) => 4'}</pre>`;
+  const TYPESCRIPT_QUASIS = `<pre data-language="typescript">{\`const a = (b: number) => 4\`}</pre>`;
+  const TYPESCRIPT_QUASIS_LINE_BREAKS = `<pre data-language="typescript">
+  
+  {\`const a = (b: number) => 4\`}
+  </pre>`;
+  const TYPESCRIPT_NO_CODE = `<pre data-language="typescript" />`;
+  const TYPESCRIPT_NO_LANGUAGE = `<pre>{'const a = (b: number) => 4'}</pre>`;
+  const TYPESCRIPT_FORMATTING_ERROR = `<pre data-language="typescript">{'const a= b:number) =>4'}</pre>`;
+  const INVALID_LANG = `<pre data-language="hypescript">{'hype'}</pre>`;
+  const SVELTE = `<pre data-language="svelte">{\`
+  <script>
+    let count = 0;
+  <\/script>
+  
+  {@html count}
+  \`}
+  </pre>`;
+  const HTML = `<pre data-language="html">{\`
+  <script>
+    let count = 0;
+  <\/script>
+  
+  {@html count}
+  \`}
+  </pre>`;
+  const TRIM_LINE_BREAKS = `<pre data-language="typescript">{\`
+  const a = (b: number) => 4
+  
+  
+  \`}</pre>`;
+  const SVELTE_MULTILINE = `<pre data-language="svelte">{\`
+  <script>
+    import Highlight from "svelte-highlight";
+    import typescript from "svelte-highlight/languages/typescript";
+    import atomOneDark from "svelte-highlight/styles/atom-one-dark";
+  
+    const code = "const add = (a: number, b: number) => a + b;";
+  </script>
+  
+  <svelte:head>
+    {@html atomOneDark}
+  </svelte:head>
+  
+  <Highlight language={typescript} {code} />\`}</pre>`;
 
-{@html count}
-\`}
-</pre>`;
-const TRIM_LINE_BREAKS = `<pre data-language="typescript">{\`
-const a = (b: number) => 4
+  test("filter – no file name", () => {
+    expect(p("content", "")).toEqual(undefined);
+  });
 
+  test("filter – skipped file", () => {
+    expect(p("content", "/node_modules/App.svelte")).toEqual(undefined);
+    expect(p("content", "/.svelte-kit/App.svelte")).toEqual(undefined);
+  });
 
-\`}</pre>`;
-const SVELTE_MULTILINE = `<pre data-language="svelte">{\`
-<script>
-  import Highlight from "svelte-highlight";
-  import typescript from "svelte-highlight/languages/typescript";
-  import atomOneDark from "svelte-highlight/styles/atom-one-dark";
+  test("filter – no content", () => {
+    expect(p("")).toEqual(undefined);
+  });
 
-  const code = "const add = (a: number, b: number) => a + b;";
-</script>
+  test("filter – custom ignore path", () => {
+    const options: Parameters<typeof p>[2] = {
+      ignorePath: (filename) => /demo/.test(filename),
+    };
 
-<svelte:head>
-  {@html atomOneDark}
-</svelte:head>
+    expect(p("content", "/node_modules/App.svelte", options)).toEqual("content");
+    expect(p("content", "/.svelte-kit/App.svelte", options)).toEqual("content");
+    expect(p("content", "/demo/App.svelte", options)).toEqual(undefined);
+  });
 
-<Highlight language={typescript} {code} />\`}</pre>`;
+  test("typescript – mustache tag (string, formatted)", () => {
+    expect(p(TYPESCRIPT_MESSY)).toEqual(p(TYPESCRIPT_STRING));
+  });
 
-const consoleLog = vi.spyOn(console, "log");
+  test("typescript – mustache tag (string)", () => {
+    expect(p(TYPESCRIPT_STRING)).toMatchSnapshot();
+  });
 
-test("filter – no file name", () => {
-  expect(p("content", "")).toEqual(undefined);
-});
+  test("typescript – mustache tag (quasis)", () => {
+    expect(p(TYPESCRIPT_QUASIS)).toMatchSnapshot();
+  });
 
-test("filter – skipped file", () => {
-  expect(p("content", "/node_modules/App.svelte")).toEqual(undefined);
-  expect(p("content", "/.svelte-kit/App.svelte")).toEqual(undefined);
-});
+  test("typescript – mustache tag (quasis with line breaks)", () => {
+    expect(p(TYPESCRIPT_QUASIS_LINE_BREAKS)).toMatchSnapshot();
+    expect(p(TYPESCRIPT_QUASIS_LINE_BREAKS)).toEqual(p(TYPESCRIPT_QUASIS));
+  });
 
-test("filter – no content", () => {
-  expect(p("")).toEqual(undefined);
-});
+  test("typescript – no code", () => {
+    expect(p(TYPESCRIPT_NO_CODE)).toMatchSnapshot();
+    expect(consoleLog).toBeCalledWith("[App.svelte]", "No code provided");
+  });
 
-test("filter – custom ignore path", () => {
-  const options: Parameters<typeof p>[2] = {
-    ignorePath: (filename) => /demo/.test(filename),
-  };
+  test("typescript – no language", () => {
+    expect(p(TYPESCRIPT_NO_LANGUAGE)).toMatchSnapshot();
+  });
 
-  expect(p("content", "/node_modules/App.svelte", options)).toEqual("content");
-  expect(p("content", "/.svelte-kit/App.svelte", options)).toEqual("content");
-  expect(p("content", "/demo/App.svelte", options)).toEqual(undefined);
-});
+  test("typescript – formatting error", () => {
+    expect(p(TYPESCRIPT_FORMATTING_ERROR)).toMatchSnapshot();
+    expect(consoleLog).toBeCalledTimes(1);
+  });
 
-test("typescript – mustache tag (string, formatted)", () => {
-  expect(p(TYPESCRIPT_MESSY)).toEqual(p(TYPESCRIPT_STRING));
-});
+  test("invalid language", () => {
+    expect(p(INVALID_LANG)).toMatchSnapshot();
+    expect(consoleLog).toBeCalledWith("[App.svelte]", 'Invalid language "hypescript"');
+  });
 
-test("typescript – mustache tag (string)", () => {
-  expect(p(TYPESCRIPT_STRING)).toMatchSnapshot();
-});
+  test("svelte", () => {
+    expect(p(SVELTE)).toMatchSnapshot();
+  });
 
-test("typescript – mustache tag (quasis)", () => {
-  expect(p(TYPESCRIPT_QUASIS)).toMatchSnapshot();
-});
+  test("svelte is an alias for html", () => {
+    expect(p(SVELTE)).toEqual(p(HTML)!.replace(/"html"/, "svelte"));
+  });
 
-test("typescript – mustache tag (quasis with line breaks)", () => {
-  expect(p(TYPESCRIPT_QUASIS_LINE_BREAKS)).toMatchSnapshot();
-  expect(p(TYPESCRIPT_QUASIS_LINE_BREAKS)).toEqual(p(TYPESCRIPT_QUASIS));
-});
+  test("trim line breaks", () => {
+    expect(p(TRIM_LINE_BREAKS)).toEqual(p(TYPESCRIPT_QUASIS));
+  });
 
-test("typescript – no code", () => {
-  expect(p(TYPESCRIPT_NO_CODE)).toMatchSnapshot();
-  expect(consoleLog).toBeCalledWith("[App.svelte]", "No code provided");
-});
+  test("multiple", () => {
+    expect(p(TYPESCRIPT_MESSY + "\n\n" + TYPESCRIPT_STRING)).toMatchSnapshot();
+  });
 
-test("typescript – no language", () => {
-  expect(p(TYPESCRIPT_NO_LANGUAGE)).toMatchSnapshot();
-});
-
-test("typescript – formatting error", () => {
-  expect(p(TYPESCRIPT_FORMATTING_ERROR)).toMatchSnapshot();
-  expect(consoleLog).toBeCalled();
-});
-
-test("invalid language", () => {
-  expect(p(INVALID_LANG)).toMatchSnapshot();
-  expect(consoleLog).toBeCalledWith("[App.svelte]", 'Invalid language "hypescript"');
-});
-
-test("svelte", () => {
-  expect(p(SVELTE)).toMatchSnapshot();
-});
-
-test("svelte is an alias for html", () => {
-  expect(p(SVELTE)).toEqual(p(HTML)!.replace(/"html"/, "svelte"));
-});
-
-test("trim line breaks", () => {
-  expect(p(TRIM_LINE_BREAKS)).toEqual(p(TYPESCRIPT_QUASIS));
-});
-
-test("multiple", () => {
-  expect(p(TYPESCRIPT_MESSY + "\n\n" + TYPESCRIPT_STRING)).toMatchSnapshot();
-});
-
-test("svelte - multiline", () => {
-  expect(p(SVELTE_MULTILINE)).toMatchSnapshot();
+  test("svelte - multiline", () => {
+    expect(p(SVELTE_MULTILINE)).toMatchSnapshot();
+  });
 });
